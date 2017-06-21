5799aabe2a84bf75a49bed9b87553cbcd64b6438
GITUSER <hudson@inboundnow.com>
2017-06-20 23:52:04 -0500

    debugging double optin list settings.

--- shared/classes/class.database-routines.php
+++ shared/classes/class.database-routines.php
@@ -139,10 +139,14 @@ if ( !class_exists('Inbound_Upgrade_Routines') ) {
             $table_name = $wpdb->prefix . "inbound_page_views";
 
             /* add ip field if does not exist */
-            $row = $wpdb->get_results(  "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{$table_name}' AND column_name = 'ip'"  );
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `ip` VARCHAR(45) NOT NULL" );
-            /* alter ip field to fix bad field types */
-            $wpdb->get_results( "ALTER TABLE {$table_name} MODIFY COLUMN `ip` VARCHAR(45)" );
+
+            $col_check = $wpdb->get_row("SELECT * FROM " . $table_name);
+
+            if(!isset($col_check->ip)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `ip` VARCHAR(45) NOT NULL");
+            } else {
+                $wpdb->get_results( "ALTER TABLE {$table_name} MODIFY COLUMN `ip` VARCHAR(45)" );
+            }
 
         }
 
@@ -157,9 +161,19 @@ if ( !class_exists('Inbound_Upgrade_Routines') ) {
             require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
             $table_name = $wpdb->prefix . "inbound_events";
 
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `funnel` text NOT NULL" );
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `source` text NOT NULL" );
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `list_id` mediumint(20) NOT NULL" );
+            $col_check = $wpdb->get_row("SELECT * FROM " . $table_name);
+
+            if(!isset($col_check->funnel)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `funnel` text NOT NULL");
+            }
+
+            if(!isset($col_check->source)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `source` text NOT NULL");
+            }
+
+            if(!isset($col_check->list_id)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `list_id` mediumint(20) NOT NULL");
+            }
 
         }
 
@@ -174,9 +188,15 @@ if ( !class_exists('Inbound_Upgrade_Routines') ) {
             require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
             $table_name = $wpdb->prefix . "inbound_events";
 
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `rule_id` mediumint(20) NOT NULL" );
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `job_id` mediumint(20) NOT NULL" );
+            $col_check = $wpdb->get_row("SELECT * FROM " . $table_name);
 
+            if(!isset($col_check->rule_id)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `rule_id` mediumint(20) NOT NULL");
+            }
+
+            if(!isset($col_check->job_id)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `job_id` mediumint(20) NOT NULL");
+            }
         }
 
 
@@ -191,8 +211,11 @@ if ( !class_exists('Inbound_Upgrade_Routines') ) {
             require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
             $table_name = $wpdb->prefix . "inbound_events";
 
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `comment_id` mediumint(20) NOT NULL" );
+            $col_check = $wpdb->get_row("SELECT * FROM " . $table_name);
 
+            if(!isset($col_check->comment_id)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `comment_id` mediumint(20) NOT NULL");
+            }
         }
 
         /**
@@ -226,8 +249,11 @@ if ( !class_exists('Inbound_Upgrade_Routines') ) {
             require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
             $table_name = $wpdb->prefix . "inbound_automation_queue";
 
-            $wpdb->get_results( "ALTER TABLE {$table_name} ADD `lead_id` mediumint(20)  NOT NULL" );
+            $col_check = $wpdb->get_row("SELECT * FROM " . $table_name);
 
+            if(!isset($col_check->lead_id)) {
+                $wpdb->get_results("ALTER TABLE {$table_name} ADD `lead_id` mediumint(20)  NOT NULL");
+            }
         }
     }
 
